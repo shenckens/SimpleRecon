6cbefcc066ffa4842c21fbb98f380336baed3e7b
shenckens <stijn.henckens@gmai.com>
2022-09-30 16:23:55 +0200

    run test with gpumem_recording

--- test.py
+++ test.py
@@ -1,66 +1,66 @@
-""" 
-    Predicts depth maps using a DepthModel model. Uses an MVS dataset from 
+"""
+    Predicts depth maps using a DepthModel model. Uses an MVS dataset from
     datasets.
-    
+
     All results will be stored at a base results folder (results_path) at:
         opts.output_base_path/opts.name/opts.dataset/opts.frame_tuple_type/
 
-    frame_tuple_type is the type of image tuple used for MVS. A selection should 
-    be provided in the data_config file you used. 
+    frame_tuple_type is the type of image tuple used for MVS. A selection should
+    be provided in the data_config file you used.
 
-    By default will compute depth scores for each frame and provide both frame 
-    averaged and scene averaged metrics. The script will save these scores (per 
+    By default will compute depth scores for each frame and provide both frame
+    averaged and scene averaged metrics. The script will save these scores (per
     scene and totals) under:
         results_path/scores
 
-    We've done our best to ensure that a torch batching bug through the matching 
-    encoder is fixed for (<10^-4) accurate testing by disabling image batching 
-    through that encoder. Run `--batch_size 4` at most if in doubt, and if 
-    you're looking to get as stable as possible numbers and avoid PyTorch 
+    We've done our best to ensure that a torch batching bug through the matching
+    encoder is fixed for (<10^-4) accurate testing by disabling image batching
+    through that encoder. Run `--batch_size 4` at most if in doubt, and if
+    you're looking to get as stable as possible numbers and avoid PyTorch
     gremlins, use `--batch_size 1`  for comparison evaluation.
 
 
     If you want to use this for speed, set --fast_cost_volume to True. This will
-    enable batching through the matching encoder and will enable an einops 
+    enable batching through the matching encoder and will enable an einops
     optimized feature volume.
 
-    This script can also be used to perform a few different auxiliary tasks, 
+    This script can also be used to perform a few different auxiliary tasks,
     including:
 
     ### TSDF Fusion
-    To run TSDF fusion provide the --run_fusion flag. You have two choices for 
+    To run TSDF fusion provide the --run_fusion flag. You have two choices for
     fusers
-    1) '--depth_fuser ours' (default) will use our fuser, whose meshes are used 
-        in most visualizations and for scores. This fuser does not support 
-        color. You must use the provided version of skimage for our custom 
-        measure.marching_cubes implementation that allows exporting a single 
+    1) '--depth_fuser ours' (default) will use our fuser, whose meshes are used
+        in most visualizations and for scores. This fuser does not support
+        color. You must use the provided version of skimage for our custom
+        measure.marching_cubes implementation that allows exporting a single
         walled mesh.
-    2) '--depth_fuser open3d' will use the open3d depth fuser. This fuser 
-        supports color and you can enable this by using the '--fuse_color' flag. 
-    
-    By default, depth maps will be clipped to 3m for fusion and a tsdf 
-    resolution of 0.04m3 will be used, but you can change that by changing both 
+    2) '--depth_fuser open3d' will use the open3d depth fuser. This fuser
+        supports color and you can enable this by using the '--fuse_color' flag.
+
+    By default, depth maps will be clipped to 3m for fusion and a tsdf
+    resolution of 0.04m3 will be used, but you can change that by changing both
     '--max_fusion_depth' and '--fusion_resolution'
 
-    You can optionnally ask for predicted depths used for fusion to be masked 
-    when no vaiid MVS information exists using '--mask_pred_depths'. This is not 
+    You can optionnally ask for predicted depths used for fusion to be masked
+    when no vaiid MVS information exists using '--mask_pred_depths'. This is not
     enabled by default.
 
-    You can also fuse the best guess depths from the cost volume before the 
-    U-Net that fuses the strong image prior. You can do this by using 
+    You can also fuse the best guess depths from the cost volume before the
+    U-Net that fuses the strong image prior. You can do this by using
     --fusion_use_raw_lowest_cost.
 
     Meshes will be stored under results_path/meshes/mesh_options/
 
     ### Cache depths
-    You can optionally store depths by providing the '--cache_depths' flag. 
+    You can optionally store depths by providing the '--cache_depths' flag.
     They will be stored at
         results_path/depths
 
     ### Quick viz
-    There are other scripts for deeper visualizations of output depths and 
-    fusion, but for quick export of depth map visualization you can use 
-    '--dump_depth_visualization'. Visualizations will be stored at 
+    There are other scripts for deeper visualizations of output depths and
+    fusion, but for quick export of depth map visualization you can use
+    '--dump_depth_visualization'. Visualizations will be stored at
         results_path/viz/quick_viz/
 
 
@@ -128,11 +128,11 @@ import modules.cost_volume as cost_volume
 def main(opts):
 
     # get dataset
-    dataset_class, scans = get_dataset(opts.dataset, 
+    dataset_class, scans = get_dataset(opts.dataset,
                         opts.dataset_scan_split_file, opts.single_debug_scan_id)
- 
+
     # path where results for this model, dataset, and tuple type are.
-    results_path = os.path.join(opts.output_base_path, opts.name, 
+    results_path = os.path.join(opts.output_base_path, opts.name,
                                         opts.dataset, opts.frame_tuple_type)
 
     # set up directories for fusion
@@ -146,7 +146,7 @@ def main(opts):
         if opts.fusion_use_raw_lowest_cost:
             mesh_output_folder_name = mesh_output_folder_name + "_raw_cv"
 
-        mesh_output_dir = os.path.join(results_path, "meshes", 
+        mesh_output_dir = os.path.join(results_path, "meshes",
                                                         mesh_output_folder_name)
 
         Path(mesh_output_dir).mkdir(parents=True, exist_ok=True)
@@ -171,7 +171,7 @@ def main(opts):
     # set up directories for quick depth visualizations
     if opts.dump_depth_visualization:
         viz_output_folder_name = "quick_viz"
-        viz_output_dir = os.path.join(results_path, "viz", 
+        viz_output_dir = os.path.join(results_path, "viz",
                                                         viz_output_folder_name)
 
         Path(viz_output_dir).mkdir(parents=True, exist_ok=True)
@@ -188,12 +188,12 @@ def main(opts):
     # Set up model. Note that we're not passing in opts as an argument, although
     # we could. We're being pretty stubborn with using the options the model had
     # used when training, saved internally as part of hparams in the checkpoint.
-    # You can change this at inference by passing in 'opts=opts,' but there 
+    # You can change this at inference by passing in 'opts=opts,' but there
     # be dragons if you're not careful.
     model = DepthModel.load_from_checkpoint(
                                 opts.load_weights_from_checkpoint,
                                 args=None)
-    if (opts.fast_cost_volume and  
+    if (opts.fast_cost_volume and
             isinstance(model.cost_volume, cost_volume.FeatureVolumeManager)):
         model.cost_volume = model.cost_volume.to_fast()
 
@@ -213,7 +213,7 @@ def main(opts):
 
         # loop over scans
         for scan in tqdm(scans):
-            
+
             # initialize fuser if we need to fuse
             if opts.run_fusion:
                 fuser = fusers_helper.get_fuser(opts, scan)
@@ -240,16 +240,16 @@ def main(opts):
                     )
 
             dataloader = torch.utils.data.DataLoader(
-                                            dataset, 
-                                            batch_size=opts.batch_size, 
-                                            shuffle=False, 
-                                            num_workers=opts.num_workers, 
+                                            dataset,
+                                            batch_size=opts.batch_size,
+                                            shuffle=False,
+                                            num_workers=opts.num_workers,
                                             drop_last=False,
                                         )
 
             # initialize scene averager
             scene_frame_metrics = ResultsAverager(
-                                                opts.name, 
+                                                opts.name,
                                                 f"scene {scan} metrics"
                                             )
 
@@ -263,23 +263,24 @@ def main(opts):
 
                 # run to get output, also measure time
                 start_time.record()
-                # use unbatched (looping) matching encoder image forward passes 
-                # for numerically stable testing. If opts.fast_cost_volume, then 
+                # use unbatched (looping) matching encoder image forward passes
+                # for numerically stable testing. If opts.fast_cost_volume, then
                 # batch.
                 outputs = model(
-                                "test", cur_data, src_data, 
+                                "test", cur_data, src_data,
                                 unbatched_matching_encoder_forward=(
                                     not opts.fast_cost_volume
-                                ), 
+                                ),
                                 return_mask=True,
                             )
                 end_time.record()
                 torch.cuda.synchronize()
 
                 elapsed_model_time = start_time.elapsed_time(end_time)
+                after_inference_mem = torch.cuda.memory_allocated(0)
 
                 upsampled_depth_pred_b1hw = F.interpolate(
-                                outputs["depth_pred_s0_b1hw"], 
+                                outputs["depth_pred_s0_b1hw"],
                                 size=(depth_gt.shape[-2], depth_gt.shape[-1]),
                                 mode="nearest",
                             )
@@ -291,14 +292,14 @@ def main(opts):
                 if (valid_mask_b).any():
                     # compute metrics
                     metrics_b_dict = compute_depth_metrics_batched(
-                        depth_gt.flatten(start_dim=1).float(), 
-                        upsampled_depth_pred_b1hw.flatten(start_dim=1).float(), 
+                        depth_gt.flatten(start_dim=1).float(),
+                        upsampled_depth_pred_b1hw.flatten(start_dim=1).float(),
                         valid_mask_b.flatten(start_dim=1),
                         mult_a=True,
                     )
 
-                    # go over batch and get metrics frame by frame to update 
-                    # the averagers 
+                    # go over batch and get metrics frame by frame to update
+                    # the averagers
                     for element_index in range(depth_gt.shape[0]):
                         if (~valid_mask_b[element_index]).all():
                             # ignore if no valid gt exists
@@ -307,9 +308,11 @@ def main(opts):
                         element_metrics = {}
                         for key in list(metrics_b_dict.keys()):
                             element_metrics[key] = metrics_b_dict[key][element_index]
-                        
+
                         # get per frame time in the batch
                         element_metrics["model_time"] = (elapsed_model_time /
+                                                            depth_gt.shape[0])
+                        element_metrics['gpu_memory'] = (after_inference_mem /
                                                             depth_gt.shape[0])
 
                         # both this scene and all frame averagers
@@ -318,33 +321,33 @@ def main(opts):
 
                 ######################### DEPTH FUSION #########################
                 if opts.run_fusion:
-                    # mask predicted depths when no vaiid MVS information 
+                    # mask predicted depths when no vaiid MVS information
                     # exists, off by default
                     if opts.mask_pred_depth:
-                        overall_mask_b1hw = outputs[    
+                        overall_mask_b1hw = outputs[
                                                     "overall_mask_bhw"
                                             ].cuda().unsqueeze(1).float()
 
                         overall_mask_b1hw = F.interpolate(
-                                overall_mask_b1hw, 
-                                size=(depth_gt.shape[-2], depth_gt.shape[-1]), 
+                                overall_mask_b1hw,
+                                size=(depth_gt.shape[-2], depth_gt.shape[-1]),
                                 mode="nearest"
                         ).bool()
 
                         upsampled_depth_pred_b1hw[~overall_mask_b1hw] = -1
-                    
-                    # fuse the raw best guess depths from the cost volume, off 
+
+                    # fuse the raw best guess depths from the cost volume, off
                     # by default
                     if opts.fusion_use_raw_lowest_cost:
-                        # upsampled_depth_pred_b1hw becomes the argmax from the 
+                        # upsampled_depth_pred_b1hw becomes the argmax from the
                         # cost volume
                         upsampled_depth_pred_b1hw = outputs[
                                                         "lowest_cost_bhw"
                                                     ].unsqueeze(1)
 
                         upsampled_depth_pred_b1hw = F.interpolate(
-                                upsampled_depth_pred_b1hw, 
-                                size=(depth_gt.shape[-2], depth_gt.shape[-1]), 
+                                upsampled_depth_pred_b1hw,
+                                size=(depth_gt.shape[-2], depth_gt.shape[-1]),
                                 mode="nearest",
                             )
 
@@ -353,21 +356,21 @@ def main(opts):
                                             ].cuda().unsqueeze(1).float()
 
                         overall_mask_b1hw = F.interpolate(
-                                overall_mask_b1hw, 
-                                size=(depth_gt.shape[-2], depth_gt.shape[-1]), 
+                                overall_mask_b1hw,
+                                size=(depth_gt.shape[-2], depth_gt.shape[-1]),
                                 mode="nearest"
                             ).bool()
 
                         upsampled_depth_pred_b1hw[~overall_mask_b1hw] = -1
-                    
-                    color_frame = (cur_data["high_res_color_b3hw"] 
-                                    if  "high_res_color_b3hw" in cur_data 
+
+                    color_frame = (cur_data["high_res_color_b3hw"]
+                                    if  "high_res_color_b3hw" in cur_data
                                      else cur_data["image_b3hw"])
 
                     fuser.fuse_frames(
-                                        upsampled_depth_pred_b1hw, 
-                                        cur_data["K_full_depth_b44"], 
-                                        cur_data["cam_T_world_b44"], 
+                                        upsampled_depth_pred_b1hw,
+                                        cur_data["K_full_depth_b44"],
+                                        cur_data["cam_T_world_b44"],
                                         color_frame
                                 )
 
@@ -379,9 +382,9 @@ def main(opts):
 
                     quick_viz_export(
                                 output_path,
-                                outputs, 
+                                outputs,
                                 cur_data,
-                                batch_ind, 
+                                batch_ind,
                                 valid_mask_b,
                                 opts.batch_size,
                             )
@@ -393,7 +396,7 @@ def main(opts):
 
                     cache_model_outputs(
                                 output_path,
-                                outputs, 
+                                outputs,
                                 cur_data,
                                 src_data,
                                 batch_ind,
@@ -404,21 +407,21 @@ def main(opts):
             # save the fused tsdf into a mesh file
             if opts.run_fusion:
                 fuser.export_mesh(
-                                    os.path.join(mesh_output_dir, 
+                                    os.path.join(mesh_output_dir,
                                         f"{scan.replace('/', '_')}.ply"),
                                 )
 
             # compute a clean average
             scene_frame_metrics.compute_final_average()
-            
-            # one scene counts as a complete unit of metrics 
+
+            # one scene counts as a complete unit of metrics
             all_scene_metrics.update_results(scene_frame_metrics.final_metrics)
 
             # print running metrics.
             print("\nScene metrics:")
             scene_frame_metrics.print_sheets_friendly(include_metrics_names=True)
             scene_frame_metrics.output_json(
-                                os.path.join(scores_output_dir, 
+                                os.path.join(scores_output_dir,
                                     f"{scan.replace('/', '_')}_metrics.json")
                             )
             # print running metrics.
@@ -433,11 +436,11 @@ def main(opts):
         all_scene_metrics.compute_final_average()
         all_scene_metrics.pretty_print_results(print_running_metrics=False)
         all_scene_metrics.print_sheets_friendly(
-                                                include_metrics_names=True, 
+                                                include_metrics_names=True,
                                                 print_running_metrics=False,
                                             )
         all_scene_metrics.output_json(
-                                os.path.join(scores_output_dir, 
+                                os.path.join(scores_output_dir,
                                     f"all_scene_avg_metrics_{opts.split}.json")
                             )
 
@@ -445,11 +448,11 @@ def main(opts):
         all_frame_metrics.compute_final_average()
         all_frame_metrics.pretty_print_results(print_running_metrics=False)
         all_frame_metrics.print_sheets_friendly(
-                                                include_metrics_names=True, 
+                                                include_metrics_names=True,
                                                 print_running_metrics=False
                                             )
         all_frame_metrics.output_json(
-                                os.path.join(scores_output_dir, 
+                                os.path.join(scores_output_dir,
                                     f"all_frame_avg_metrics_{opts.split}.json")
                             )
 
